d91ff4025e13ba8ddc315991281e5e15c2295376
Lee Daniel <dany@Lees-MacBook-Air-2.local>
2023-10-14 21:14:56 +0900

    Add: TSP

--- 10971/TSP.cpp
+++ 10971/TSP.cpp
@@ -2,25 +2,31 @@
 #include <iostream>
 #include <vector>
 
-int W[11][11];
-int path;
+int W[11][11];        // city board
+int result = INT_MAX; // total result;
 
-void getPath(std::vector<bool> &visit, int &N, int sum, int from) {
-  if (from == N) {
-    if (sum < path)
-      path = sum;
+void getPath(std::vector<int> &path, std::vector<bool> &visit, int &N, int sum,
+             int from) {
+  if (path.size() == N) {
+    sum += W[from][path[0]]; // sum last city to first city;
+    if (sum < result) {
+      result = sum; // change the shortest path;
+    }
     return;
   }
   for (int i = 1; i <= N; i++) {
-    if (from == 0) {
-      visit[i] = true;
-      getPath(visit, N, sum, i);
-    } else if (visit[i] == true || i == from) {
+    if (visit[i] == true || i == from) {
       continue;
     } else {
-      visit[i] = true;
-      getPath(visit, N, sum + W[from][i], i);
-      visit[i] = false;
+      visit[i] = true;   // check visit flag;
+      path.push_back(i); // push the number of the cisy list;
+      if (from == 0) {
+        getPath(path, visit, N, sum, i); // first recursive;
+      } else {
+        getPath(path, visit, N, sum + W[from][i], i);
+      }
+      path.pop_back();  // reset the number of city;
+      visit[i] = false; // reset the visit flag;
     }
   }
 }
@@ -29,55 +35,22 @@ int main() {
   int N;
   std::cin >> N;
 
+  // fill city board;
   for (int i = 1; i <= N; i++) {
     for (int j = 1; j <= N; j++) {
       std::cin >> W[i][j];
     }
   }
 
+  // fill visit flag;
   std::vector<bool> visit;
   for (int i = 0; i <= N; i++) {
     visit.push_back(false);
   }
 
-  path = INT_MAX;
-  getPath(visit, N, 0, 0);
+  std::vector<int> path; // number of the city list;
+  getPath(path, visit, N, 0, 0);
 
-  // for (int i = 1; i <= 4; i++) {
-  //   int sum = 0;
-  //   visit[i] = true;
-  //   for (int j = 1; j <= 4; j++) {
-  //     if (visit[j] == true || i == j)
-  //       continue;
-  //     else {
-  //       visit[j] = true;
-  //       sum += W[i][j];
-  //     }
-  //     for (int k = 1; k <= 4; k++) {
-  //       if (visit[k] == true || j == k)
-  //         continue;
-  //       else {
-  //         visit[k] = true;
-  //         sum += W[j][k];
-  //       }
-  //       for (int l = 1; l <= 4; l++) {
-  //         if (visit[l] == true || k == l)
-  //           continue;
-  //         else {
-  //           visit[l] = true;
-  //           sum += W[k][l];
-  //           sum += W[l][i];
-  //           if (sum < path)
-  //             path = sum;
-  //           visit[l] = false;
-  //         }
-  //       }
-  //       visit[k] = false;
-  //     }
-  //     visit[j] = false;
-  //   }
-  //   visit[i] = false;
-  // }
-  std::cout << path;
+  std::cout << result;
   return 0;
 }
