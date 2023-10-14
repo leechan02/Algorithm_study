9869e73f76b44de124b7eb44d85462936700cba8
Lee Daniel <dany@Lees-MacBook-Air-2.local>
2023-10-14 15:26:57 +0900

    Finish: porblem solved

--- 1018/Chess.cpp
+++ 1018/Chess.cpp
@@ -10,7 +10,7 @@ int main() {
   std::cin >> N >> M;
 
   // char board[N][M];
-  std::vector<std::vector<char>> board(N, std::vector<char>(M));
+  std::vector<std::vector<char> > board(N, std::vector<char>(M));
 
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < M; j++) {
@@ -18,40 +18,38 @@ int main() {
     }
   }
 
-  int max = 64;                                 // 8 * 8 chess board;
-  int whiteFirst[8] = {1, 0, 1, 0, 1, 0, 1, 0}; // whitecheck block;
-  int blackfirst[8] = {0, 1, 0, 1, 0, 1, 0, 1}; // blackcheck block;
+  int max = 64; // 8 * 8 chess board;
+  char whiteFirst[8] = {'W', 'B', 'W', 'B',
+                        'W', 'B', 'W', 'B'}; // whitecheck block;
+  char blackfirst[8] = {'B', 'W', 'B', 'W',
+                        'B', 'W', 'B', 'W'}; // blackcheck block;
 
-  for (int i = 0; i + 8 < N; i++) {
-    for (int j = 0; j + 8 < M; j++) {
-      int sum = 0; // painted block;
-      int n = 0;
-      bool color;
-      if (board[i][j] == 1) {
-        color = WHITE;
-      } else {
-        color = BLACK;
-      }
-      for (int k = i; k < i + 8; k++) {
-        int m = 0;
-        for (int l = j; l < j + 8; l++) {
-          if (color == WHITE) {
-            if (m % 2 == 0 && board[k][l] == 'W') {
-              sum++;
-            } else if (m % 1 == 1 && board[k][l] == 'B') {
-              sum++;
-            }
-          } else if (color == BLACK) {
-            if (m % 2 == 0 && board[k][l] == 'W') {
-              sum++;
-            } else if (m % 1 == 1 && board[k][l] == 'B') {
-              sum++;
+  for (int i = 0; i + 8 <= N; i++) {
+    for (int j = 0; j + 8 <= M; j++) {
+      bool color = WHITE;
+      for (int c = 0; c < 2; c++) {
+        int sum = 0; // painted block;
+        for (int k = i, n = 0; k < i + 8; k++, n++) {
+          for (int l = j, m = 0; l < j + 8; l++, m++) {
+            if (color == WHITE) {
+              if (board[k][l] != whiteFirst[m]) {
+                sum++;
+              }
+            } else if (color == BLACK) {
+              if (board[k][l] != blackfirst[m]) {
+                sum++;
+              }
             }
           }
+          color ^= 1;
         }
-        color |= 1;
+        if (sum < max) {
+          max = sum;
+        }
+        color = BLACK;
       }
     }
   }
+  std::cout << max;
   return 0;
 }
