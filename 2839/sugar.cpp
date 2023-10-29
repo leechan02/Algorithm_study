fcf83d45e6ef20205a85458b575e9197c4f20c02
Lee Daniel <dany@Lees-MacBook-Air-2.local>
2023-10-29 21:13:41 +0900

    Add: 2839

---
+++ 2839/sugar.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+  int N;
+
+  std::cin >> N;
+
+  int sum = 0;
+  for (int i = 0; i < N; i += 3) {
+    for (int j = i; j < N; i += 5) {
+      
+    }
+  }
+  return (0);
+}
