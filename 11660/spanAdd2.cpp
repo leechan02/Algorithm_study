48f009327ad246bd9410134346c32a3fb818f933
euiclee <lechan1317@gmail.com>
2023-11-30 10:24:19 +0900

    Fix: time

--- 11660/spanAdd2.cpp
+++ 11660/spanAdd2.cpp
@@ -14,18 +14,15 @@ int main() {
     S[i][0] = 0;
     for (int j = 1; j <= N; ++j) {
       std::cin >> arr[i][j];
-      S[i][j] = S[i][j - 1] + arr[i][j];
+      S[i][j] = S[i][j - 1] + S[i - 1][j] - S[i - 1][j - 1] + arr[i][j];
     }
   }
 
-  int X1, Y1, X2, Y2, result;
+  int X1, Y1, X2, Y2;
   for (int k = 0; k < M; ++k) {
     std::cin >> X1 >> Y1 >> X2 >> Y2;
-    result = 0;
-    for (; X1 <= X2; ++X1) {
-      result += S[X1][Y2] - S[X1][Y1 - 1];
-    }
-    std::cout << result << '\n';
+    std::cout << S[X2][Y2] - S[X1 - 1][Y2] - S[X2][Y1 - 1] + S[X1 - 1][Y1 - 1]
+              << '\n';
   }
   return (0);
 }
