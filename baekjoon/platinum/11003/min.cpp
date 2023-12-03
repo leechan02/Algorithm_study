5c67781281f8a030a7cc48ac13b6c2c739fb88d9
leechan02 <lechan1317@gmail.com>
2023-12-03 18:09:56 +0900

    Solved: 11003

---
+++ baekjoon/platinum/11003/min.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <deque>
+
+int main() {
+  std::ios::sync_with_stdio(0);
+  std::cin.tie(0);
+  std::cout.tie(0);
+
+  int N, L;
+  std::cin >> N >> L;
+  std::deque<std::pair<int, int> >  deque;
+
+  for (int i = 0; i < N; ++i) {
+    int now;
+    std::cin >> now;
+
+    while (deque.size() && deque.back().first > now) {
+      deque.pop_back();
+    }
+    deque.push_back(std::make_pair(now, i));
+
+    if (deque.front().second <= i - L) {
+      deque.pop_front();
+    }
+    std::cout << deque.front().first << " ";
+  }
+  return (0);
+}
