d8cb3e580cd28f4bc41ff70c9133f7bfc5a413eb
leechan02 <lechan1317@gmail.com>
2023-12-07 17:58:56 +0900

    Solved: 2751 book

---
+++ baekjoon/silver/2751/sort.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> arr;
+std::vector<int> sorted;
+
+void merge(int left, int right) {
+  int mid = (left + right) / 2;
+  int i = left;
+  int j = mid + 1;
+  int k = left;
+
+  while (i <= mid && j <= right) {
+    if (arr[i] > arr[j]) {
+      sorted[k++] = arr[j++];
+    } else {
+      sorted[k++] = arr[i++];
+    }
+  }
+
+  if (i > mid) {
+    while (j <= right) {
+      sorted[k++] = arr[j++];
+    }
+  } else {
+    while (i <= mid) {
+      sorted[k++] = arr[i++];
+    }
+  }
+
+  for (int a = left; a <= right; a++) {
+    arr[a] = sorted[a];
+  }
+}
+
+void mergeSort(int left, int right) {
+  int mid;
+
+  if (left < right) {
+    mid = (left + right) / 2;
+    mergeSort(left, mid);
+    mergeSort(mid + 1, right);
+    merge(left, right);
+  }
+}
+
+int main() {
+  std::ios::sync_with_stdio(0);
+  std::cin.tie(0);
+  std::cout.tie(0);
+
+  int N;
+  std::cin >> N;
+
+  arr = std::vector<int>(N, 0);
+  sorted = std::vector<int>(N, 0);
+
+  for (int i = 0; i < N; ++i) {
+    std::cin >> arr[i];
+  }
+
+  mergeSort(0, N - 1);
+
+  for (int i = 0; i < N; ++i) {
+    std::cout << arr[i] << "\n";
+  }
+  return (0);
+}
