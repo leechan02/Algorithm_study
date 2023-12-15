d7dc67e120716e649f9a69d8daa3935a8387fc69
leechan02 <lechan1317@gmail.com>
2023-12-15 23:02:55 +0900

    Solved: 1744 yim

--- baekjoon/gold/1744/num.cpp
+++ baekjoon/gold/1744/num.cpp
@@ -12,7 +12,20 @@ int main() {
   }
   std::sort(arr.begin(), arr.end());
 
-  int ans = 0, sum;
+  int ans = 0, sum, first = 0, last = N - 1;
+  while (first + 1 < N - 1 && arr[first + 1] <= 0) {
+    sum = arr[first] * arr[first + 1];
+    ans += sum;
+    first += 2;
+  }
+  while (first - 2 < last - 1 && arr[last - 1] > 1) {
+    sum = arr[last] * arr[last - 1];
+    ans += sum;
+    last -= 2;
+  }
+  while (first <= last) {
+    ans += arr[first++];
+  }
   std::cout << ans << '\n';
   return (0);
 }
