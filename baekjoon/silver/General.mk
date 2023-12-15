9288e1989d7f9bc53e6cb06550a0b84f989c7ae9
euiclee <lechan1317@gmail.com>
2023-12-15 09:55:14 +0900

    Feat: std=c++11

--- baekjoon/silver/General.mk
+++ baekjoon/silver/General.mk
@@ -1,7 +1,7 @@
 CPP_FILES = $(wildcard *.cpp)
 NAME = $(basename $(CPP_FILES))
 
-CXXFLAGS = -MMD
+CXXFLAGS = -MMD -std=c++11
 OBJ_DIR = objs
 OBJS = $(addprefix $(OBJ_DIR)/, $(SRCS:.cpp=.o))
 DEF = $(addprefix $(OBJ_DIR)/, $(SRCS:.cpp=.d))
