2b647230c121348f82a69f9c5756b476f7ce3cc7
euiclee <lechan1317@gmail.com>
2023-12-22 16:18:24 +0900

    Add: 1707

---
+++ baekjoon/gold/1707/2minute.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+int main() {
+  int k;
+  std::cin >> k;
+
+  while (k--) {
+    int v, e;
+    std::cin >> v >> e;
+  }
+  return (0);
+}
