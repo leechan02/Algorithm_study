cc7249bae9e17c29a091f67e725ea12184cb0289
euiclee <lechan1317@gmail.com>
2023-12-28 18:48:08 +0900

    Solved: 1516 book

--- baekjoon/gold/1516/sejokim.cpp
+++ baekjoon/gold/1516/sejokim.cpp
@@ -1,53 +1,47 @@
 #include <iostream>
+#include <queue>
 #include <vector>
 
-std::vector<int> T;
-std::vector<int> arr[501];
-std::vector<int> dp(501, 0);
-std::vector<bool> visited(501, false);
-int max = 0;
-
-void DFS(int index, int sum) {
-  if (arr[index].size() == 0) {
-    if (sum > max) {
-      max = sum;
-    }
-    return;
-  }
-
-  visited[index] = true;
-  for (int i : arr[index]) {
-    if (!visited[i]) {
-      DFS(i, sum + T[i]);
-    }
-  }
-  visited[index] = false;
-}
-
 int main() {
-  std::ios::sync_with_stdio(0);
-  std::cin.tie(0);
-  std::cout.tie(0);
-
   int n;
   std::cin >> n;
 
-  T = std::vector<int>(n + 1, 0);
+  std::vector<int> arr[n + 1];
+  std::vector<int> D(n + 1, 0);
+  std::vector<int> T(n + 1, 0);
   for (int i = 1; i <= n; ++i) {
     int time, num;
     std::cin >> time >> num;
     T[i] = time;
     while (num > 0) {
-      arr[i].push_back(num);
+      arr[num].push_back(i);
+      D[i]++;
       std::cin >> num;
     }
   }
 
+  std::queue<int> queue;
   for (int i = 1; i <= n; ++i) {
-    visited.assign(501, false);
-    max = 0;
-    DFS(i, T[i]);
-    std::cout << max << '\n';
+    if (D[i] == 0) {
+      queue.push(i);
+    }
+  }
+
+  std::vector<int> res(n + 1, 0);
+  while (!queue.empty()) {
+    int now = queue.front();
+    queue.pop();
+
+    for (int next : arr[now]) {
+      --D[next];
+      res[next] = std::max(res[next], res[now] + T[now]);
+      if (D[next] == 0) {
+        queue.push(next);
+      }
+    }
+  }
+  for (int i = 1; i <= n; ++i) {
+    std::cout << res[i] + T[i] << '\n';
   }
   return (0);
 }
