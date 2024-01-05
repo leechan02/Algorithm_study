1533bb851f4bbfbfb803e76964d76ae4dbd48c5b
euiclee <lechan1317@gmail.com>
2024-01-05 09:12:15 +0900

    Feat: tri string

--- baekjoon/silver/14425/string.cpp
+++ baekjoon/silver/14425/string.cpp
@@ -1,26 +1,66 @@
 #include <iostream>
-#include <map>
+
+class Node {
+  public:
+    Node *next[26];
+    bool isEnd;
+  
+  public:
+    Node() : isEnd(false) {
+      std::fill(next, next + 26, nullptr);
+    }
+
+    ~Node() {
+      for (auto& child : next) {
+        delete child;
+      }
+    }
+
+    void insert(const char *key) {
+      if (*key == 0) {
+        isEnd = true;
+      } else {
+        int nextIndex = *key - 'a';
+        if (next[nextIndex] == nullptr) {
+          next[nextIndex] = new Node();
+        }
+        next[nextIndex]->insert(key + 1);
+      }
+    }
+
+    Node *find(const char *key) {
+      if (*key == 0) {
+        return this;
+      }
+      int nextIndex = *key - 'a';
+
+      if (next[nextIndex] == nullptr) {
+        return nullptr;
+      }
+      return next[nextIndex]->find(key + 1);
+    }
+};
 
 int main() {
-  int n, s;
-  std::cin >> n >> s;
+  int n, m;
+  std::cin >> n >> m;
+  Node *root = new Node();
 
-  std::map<std::string, int> dic;
-  for (int i = 0; i < n; ++i) {
-    std::string tmp;
-    std::cin >> tmp;
-    dic[tmp] = 1;
+  while (n--) {
+    char text[501];
+    std::cin >> text;
+    root->insert(text);
   }
 
-  int result = 0;
-  for (int i = 0; i < s; ++i) {
-    std::string tmp;
-    std::cin >> tmp;
+  int count = 0;
+  while (m--) {
+    char text[501];
+    std::cin >> text;
+    Node *result = root->find(text);
 
-    if (dic[tmp] == 1) {
-      ++result;
+    if (result && result->isEnd) {
+      count++;
     }
   }
-  std::cout << result << "\n";
-  return (0);
+  std::cout << count << "\n";
 }
