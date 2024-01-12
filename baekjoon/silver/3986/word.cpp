d9426623828d00434ed720f49dddc8ce4b993aad
euiclee <lechan1317@gmail.com>
2024-01-12 09:48:03 +0900

    Solved: 3986 teacher

--- baekjoon/silver/3986/word.cpp
+++ baekjoon/silver/3986/word.cpp
@@ -4,35 +4,24 @@
 int n, cnt;
 std::string str;
 
-void remove(std::string &str) {
-  std::stack<char> st;
-  for (char c : str) {
-    if (!st.empty() && st.top() == c) {
-      st.pop();
-    } else {
-      st.push(c);
-    }
-  }
-  str = "";
-  while (!st.empty()) {
-    str = st.top() + str;
-    st.pop();
-  }
-}
-
 int main() {
   std::cin >> n;
 
   for (int i = 0; i < n; ++i) {
     std::cin >> str;
-    if (str.size() & 1) {
-      continue;
+
+    std::stack<char> stack;
+    for (char c : str) {
+      if (!stack.empty() && stack.top() == c) {
+        stack.pop();
+      } else {
+        stack.push(c);
+      }
     }
-    remove(str);
-    if (str == "") {
+    if (stack.size() == 0) {
       ++cnt;
     }
   }
-  std::cout << cnt << '\n';
-  return 0;
+  std::cout << cnt << "\n";
+  return (0);
 }
