b6ba5a1e792ec3477bbb1852029d83a56b520706
leechan02 <lechan1317@gmail.com>
2024-01-14 17:48:13 +0900

    Add 1012

---
+++ baekjoon/silver/1012/b.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+typedef std::pair<int, int> position;
+std::vector<int> map[50];
+std::vector<int> visited[50];
+
+int main() {
+  int t;
+  std::cin >> t;
+  while (t--) {
+    int m, n, k;
+    std::cin >> m >> n >> k;
+
+  }
+}
