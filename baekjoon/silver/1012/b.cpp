d8d9b503365236c77a24c65eaa911048bf6fb070
euiclee <lechan1317@gmail.com>
2024-01-15 10:31:28 +0900

    Solved: 1012

--- baekjoon/silver/1012/b.cpp
+++ baekjoon/silver/1012/b.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-#include <vector>
+#include <cstring>
 
-typedef std::pair<int, int> position;
-std::vector<int> map[50];
-std::vector<int> visited[50];
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {1, -1, 0, 0};
+int map[51][51];
+int visited[51][51];
+
+void DFS(int y, int x) {
+  visited[y][x] = 1;
+  for (int i = 0; i < 4; ++i) {
+    int newY = y + dy[i];
+    int newX = x + dx[i];
+    if (newY >= 0 && newX >= 0 && map[newY][newX] == 1 &&
+        visited[newY][newX] == 0) {
+      DFS(newY, newX);
+    }
+  }
+}
 
 int main() {
   int t;
@@ -12,5 +25,22 @@ int main() {
     int m, n, k;
     std::cin >> m >> n >> k;
 
+    memset(map, 0, sizeof(map));
+    memset(visited, 0, sizeof(visited));
+    while (k--) {
+      int x, y;
+      std::cin >> x >> y;
+      map[y][x] = 1;
+    }
+    int cnt = 0;
+    for (int y = 0; y < n; ++y) {
+      for (int x = 0; x < m; ++x) {
+        if (map[y][x] == 1 && visited[y][x] == 0) {
+          ++cnt;
+          DFS(y, x);
+        }
+      }
+    }
+    std::cout << cnt << "\n";
   }
 }
