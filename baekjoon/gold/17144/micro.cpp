e456f880c98b2446f0269b28b2febdc07b45e13e
euiclee <lechan1317@gmail.com>
2024-02-08 09:27:59 +0900

    Solved: 17144

--- baekjoon/gold/17144/micro.cpp
+++ baekjoon/gold/17144/micro.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+#include <vector>
+#include <cstring>
 
 int n, m, t;
 int map[51][51];
+int tmp[51][51];
 int dy[4] = {1, -1, 0, 0};
 int dx[4] = {0, 0, -1, 1};
-typedef std::pair<int, int> pos;
-std::vector<pos> air;
+std::vector<int> air;
 
 void spread() {
+  memset(tmp, 0, sizeof(tmp));
   for (int y = 0; y < n; ++y) {
     for (int x = 0; x < m; ++x) {
       if (map[y][x] != 0 && map[y][x] != -1) {
@@ -17,23 +20,51 @@ void spread() {
           int nx = x + dx[i];
           if (ny >= 0 && ny < n && nx >= 0 && nx < m && map[ny][nx] != -1) {
             ++cnt;
-            map[ny][nx] += map[y][x] / 5;
+            tmp[ny][nx] += map[y][x] / 5;
           }
         }
         map[y][x] -= (map[y][x] / 5) * cnt;
       }
     }
   }
-}
-
-void print() {
-  std::cout << "\n";
   for (int y = 0; y < n; ++y) {
     for (int x = 0; x < m; ++x) {
-      std::cout << map[y][x] << " ";
+      if (tmp[y][x] != 0) {
+        map[y][x] += tmp[y][x];
+      }
     }
-    std::cout << "\n";
   }
+}
+
+void conditioner() {
+    int sy = air[0];
+    for (int i = sy - 1; i > 0; --i) {
+      map[i][0] = map[i - 1][0];
+    }
+    for (int i = 0; i < m; ++i) {
+      map[0][i] = map[0][i + 1];
+    }
+    for (int i = 0; i < sy; ++i) {
+      map[i][m - 1] = map[i + 1][m - 1];
+    }
+    for (int i = m; i > 1; --i) {
+      map[sy][i] = map[sy][i - 1];
+    }
+    map[sy][1] = 0;
+    sy = air[1];
+    for (int i = sy + 1; i < n; ++i) {
+      map[i][0] = map[i + 1][0];
+    }
+    for (int i = 0; i < m; ++i) {
+      map[n - 1][i] = map[n - 1][i + 1];
+    }
+    for (int i = n; i > sy; --i) {
+      map[i][m - 1] = map[i - 1][m - 1];
+    }
+    for (int i = m; i > 1; --i) {
+      map[sy][i] = map[sy][i - 1];
+    }
+    map[sy][1] = 0;
 }
 
 int main() {
@@ -43,18 +74,15 @@ int main() {
       int tmp;
       std::cin >> tmp;
       if (tmp == -1) {
-        air.push_back({y, x});
+        air.push_back(y);
       }
       map[y][x] = tmp;
     }
   }
-  print();
-
   while (t--) {
     spread();
-    // conditioner();
+    conditioner();
   }
-  print();
 
   int left_dust = 0;
   for (int y = 0; y < n; ++y) {
