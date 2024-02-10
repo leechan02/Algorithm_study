255b9f1dec036e1fe60c32ee9a6e364b896567ea
leechan02 <lechan1317@gmail.com>
2024-02-10 11:00:01 +0900

    Add: 17825

---
+++ baekjoon/gold/17825/game.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+int dice[10];
+std::vector<int> map[22];
+
+void init() {
+  for (int i)
+}
+
+int main() {
+  for (int i = 0; i < 10; ++i) {
+    std::cin >> dice[i];
+  }
+  init();
+}
