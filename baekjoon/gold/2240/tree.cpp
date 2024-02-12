39f5b49c1f9d5665280ec14ffd2012eb2f664b68
euiclee <lechan1317@gmail.com>
2024-02-13 08:39:16 +0900

    Add: 2240

---
+++ baekjoon/gold/2240/tree.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+
+int t, w, plum[1001], dp;
+
+int dfs(int start) {
+  if (start == w) {
+    return 
+  }
+}
+
+int main() {
+  std::cin >> t >> w;
+  for (int i = 0; i < t; ++i) {
+    std::cin >> plum[i];
+  }
+  std::cout << dfs(0) << "\n";
+}
