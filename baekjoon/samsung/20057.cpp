1f8f2f80fcaf2f2a99c1d8592a2d89e9e7dd3043
euiclee <lechan1317@gmail.com>
2024-02-22 08:24:00 +0900

    Solved: 20057

--- baekjoon/samsung/20057.cpp
+++ baekjoon/samsung/20057.cpp
@@ -2,9 +2,8 @@
 #include <cstring>
 #include <vector>
 
-int n, sy, sx, left, total;
+int n, sy, sx, ny, nx, left, total;
 int map[500][500];
-int tmp[500][500];
 int dy[4] = {0, 1, 0, -1};
 int dx[4] = {-1, 0, 1, 0};
 struct pos {
@@ -24,32 +23,26 @@ void checkLeft() {
 }
 
 void spread(int i, int j, int d) {
-  memset(tmp, 0, sizeof(tmp));
-  tmp[i][j] = map[i][j];
+  int now = map[i][j];
   for (auto t : dust[d]) {
-    int ny = i + t.y;
-    int nx = j + t.x;
+    ny = i + t.y;
+    nx = j + t.x;
+    int wind = now * t.p / 100;
     if (ny < 0 || ny >= n || nx < 0 || nx >= n){
-      int minus = map[i][j] * t.p / 100;
-      tmp[i][j] -= minus;
+      map[i][j] -= wind;
       continue;
     }
-    tmp[ny][nx] = map[i][j] * t.p / 100;
-    tmp[i][j] -= tmp[ny][nx];
+    map[ny][nx] += wind;
+    map[i][j] -= wind;
   }
-  int ny = i + dy[d];
-  int nx = j + dx[d];
-  if (ny >= 0 && ny < n && nx >= 0 && nx < n) tmp[ny][nx] = tmp[i][j];
-  tmp[i][j] = 0;
+  ny = i + dy[d];
+  nx = j + dx[d];
+  if (ny >= 0 && ny < n && nx >= 0 && nx < n) map[ny][nx] += map[i][j];
   map[i][j] = 0;
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < n; ++j) {
-      map[i][j] += tmp[i][j];
-    }
-  }
 }
 
 int main() {
+  std::ios::sync_with_stdio(0);std::cout.tie(0);std::cin.tie(0);
   std::cin >> n;
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < n; ++j) {
