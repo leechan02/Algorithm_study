7fdf04e6b1babdf6eca226e117e7b144ac05628b
euiclee <lechan1317@gmail.com>
2024-02-26 08:24:02 +0900

    Fix: npos

--- baekjoon/SM/split.cpp
+++ baekjoon/SM/split.cpp
@@ -5,8 +5,8 @@ std::vector<std::string> split(std::string &str, std::string sep) {
   std::vector<std::string> ret;
   int pos = 0;
   while (pos < str.size()) {
-    int next = str.find(sep, pos);
-    if (next == -1) next = str.size();
+    auto next = str.find(sep, pos);
+    if (next == std::string::npos) next = str.size();
     if (next - pos > 0) ret.push_back(str.substr(pos, next - pos));
     pos = next + sep.size();
   }
