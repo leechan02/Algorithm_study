d194c010943a32e3228c52b79b9e517fd42491cd
leechan02 <lechan1317@gmail.com>
2024-02-27 23:02:03 +0900

    Add: 15312

---
+++ baekjoon/silver/15312.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int ret;
+int alphabet[26] = {3, 2, 1, 2, 3, 3, 2, 3, 3, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 1, 1, 1, 2, 2, 1};
+int num[4004];
+std::string a, b, s, ans;
+
+int main() {
+  std::cin >> a >> b;
+
+  for (int i = 0; i < a.size(); ++i) {
+    s += a[i];
+    s += b[i];
+  }
+
+  for (int i = 0; i < s.size(); ++i) {
+    num[i] = alphabet[s[i] - 'A'];
+  }
+
+  for (int i = 1; i < s.size() / 2; ++i) {
+    for (int j = 0; j < s.size() - i; j += i) {
+      num[j] = (num[j] + num[j + i]) % 10;
+    }
+    std::cout << "\n";
+  }
+  ans = std::to_string(num[0]) + std::to_string(num[2]);
+  std::cout << ans << "\n";
+}
