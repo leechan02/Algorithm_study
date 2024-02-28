6450d74e50b220f4e4ec2c2b6e979aed779915ac
euiclee <lechan1317@gmail.com>
2024-02-29 08:34:31 +0900

    Add: 19236

--- baekjoon/samsung/19236.cpp
+++ baekjoon/samsung/19236.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <algorithm>
 
-int n, t, max;
+int n, t, max, tmpIdx;
 struct fish{
   int idx, dir, y, x;
 
@@ -28,10 +28,10 @@ void print() {
 void moveFishes() {
   int y, x, ny, nx, ty, tx, i;
   for (auto f : fishes) {
+    if (f.idx == 0) continue;
     y = f.y;
     x = f.x;
 
-    std::cout << "\n" << "before: " << f.idx;
     i = f.dir;
     do {
       ny = y + dy[i];
@@ -43,20 +43,31 @@ void moveFishes() {
       break;
     } while (i != f.dir);
     if (ny < 0 || ny >= 4 || nx < 0 || nx >= 4 || map[ny][nx] == -1) continue;
-    fishes[map[ny][nx] - 2].y = y, fishes[map[ny][nx] - 2].x = x;
-    fishes[map[y][x] - 2].y = ny, fishes[map[y][x] - 2].x = nx, fishes[map[y][x] - 2].dir = i;
-    std::cout << "\n" << "b: " << y << " " << x << " n: " << ny << " " << nx << " idx: " << map[y][x];
+    if (map[ny][nx] != 0) {
+      fishes[map[ny][nx] - 1].y = y, fishes[map[ny][nx] - 1].x = x;
+    }
+    fishes[map[y][x] - 1].y = ny, fishes[map[y][x] - 1].x = nx, fishes[map[y][x] - 1].dir = i;
     std::swap(map[y][x], map[ny][nx]);
-    print();
   }
 }
 
 void moveShark(int y, int x, int sum, int d) {
-  if (y < 0 || y >= 4 || x < 0 || x >= 4) {
-    max = std::max(sum, max);
-    return ;
-  }
+  max = std::max(sum, max);
   moveFishes();
+  int tmpFish;
+  for (int i = 1; i <= 4; ++i) {
+    int ny = y + dy[d] * i;
+    int nx = x + dx[d] * i;
+    if (ny < 0 || ny >= 4 || nx < 0 || nx >= 4 || map[ny][nx] == 0) return;
+    tmpFish = map[ny][nx];
+    map[y][x] = 0;
+    map[ny][nx] = -1;
+    fishes[tmpFish - 1].idx = 0;
+    moveShark(ny, nx, sum + tmpFish, fishes[tmpFish - 1].dir);
+    fishes[tmpFish - 1].idx = tmpFish;
+    map[ny][nx] = tmpFish;
+    map[y][x] = -1;
+  }
 }
 
 int main() {
@@ -65,12 +76,13 @@ int main() {
     for (int j = 0; j < 4; ++j) {
       std::cin >> n >> t;
       map[i][j] = n;
-      if (!(i == 0 && j == 0)) fishes.push_back({n, t - 1, i, j});
-      else shark = n, sharkDir = t;
+      fishes.push_back({n, t - 1, i, j});
+      if (i == 0 && j == 0) shark = n, sharkDir = t;
     }
   }
-  map[0][0] = -1;
   std::sort(fishes.begin(), fishes.end());
+  fishes[map[0][0] - 1].idx = 0;
+  map[0][0] = -1;
   moveShark(0, 0, shark, sharkDir);
   std::cout << max << "\n";
 }
